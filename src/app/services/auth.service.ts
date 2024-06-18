c9668a82e21bbbfd1ce59d76012ffe715c39ebec
Camilo Del Valle <delvallec@globalhitss.com>
2024-06-18 16:20:44 -0500

    OK hasta clase 5, registro, query params, etc

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiURL = environment.API_URL
+
+  constructor(
+    private http: HttpClient
+    
+  ) { 
+    
+  }
+
+  login(email: string, password: string){
+    return this.http.post(`${this.apiURL}/api/v1/auth/login`, {
+      email,
+      password
+    })
+  }
+
+  register(name: string, email: string, password: string){
+    return this.http.post(`${this.apiURL}/api/v1/auth/register`, {
+      name,
+      email,
+      password
+    })
+  }
+
+  isAvailable(email: string){
+    return this.http.post<{isAvailable: boolean}>(`${this.apiURL}/api/v1/auth/is-available`, {
+      email,
+    })
+  }
+}
